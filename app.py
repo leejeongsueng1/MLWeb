a36874eb719ca216849fe1f48aad67b59eaaf523
leejeongsueng1 <jki1256@hanmail.net>
2022-04-15 12:13:14 +0900

    hi

---
+++ app.py
@@ -0,0 +1,44 @@
+import os
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+@app.route('/mnist',methods=['GET','POST'])
+def mnist():
+    if request.method == 'GET':
+        return render_template('mnistform.html')
+    else:
+        f = request.files['mnistfile']
+        path = os.path.dirname(__file__) + '/upload/'+f.filename
+        f.save(path)
+        return "성공!!"
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
